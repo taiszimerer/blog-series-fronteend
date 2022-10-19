d9d2b2db06b10c5fb56f19bdb6decf163c778f03
taiszimerer <taiszimerer@gmail.com>
2022-10-19 13:54:53 -0300

    primeira parte semana 6 realizada

---
+++ index.js
@@ -0,0 +1,26 @@
+series = [
+    { 
+       titulo: "Outlander",
+       temporadas: 6,
+       temNaNetflix: true,
+       imagem: "outlander",
+       personagensPrincipais: ["Claire Fraser", "Jamie Fraser", "Jonathan Randall"]
+      },
+
+      { 
+     titulo: "The Handmands Tale",
+       temporadas: 5,
+       temNaNetflix: false,
+       imagem: "hand",
+       personagensPrincipais: ["June Osborne", "Serena Joy","Comandante"]
+      },
+
+      { 
+       titulo: "Greys Anatomy",
+       temporadas: 19,
+       temNaNetflix: false,
+       imagem: "greys",
+       personagensPrincipais: ["Meredith Grey", "Derek Shepherd", "Cristina Yang"]
+      },
+    ]
+console.log(series)
